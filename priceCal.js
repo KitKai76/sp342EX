430d3df8c944076be4f71d6f8ed92a987ea514e2
Patchara <Mmdakito01@gmail.com>
2023-04-27 12:06:13 +0700

    Add Calculate Lisa Con.

--- priceCal.js
+++ priceCal.js
@@ -1,9 +1,17 @@
 function priceCalculate() {
-  let ticknum = (document.getElementById("ticknum").value).trim();
+  let ticknum = parseInt(document.getElementById("ticknum").value.trim());
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
   
-  return 0;
+  let price = 100;
+  if (eventId === 4) {
+    price = 100;
+  } else if (eventId === 4) {
+    price = 200;
+  } else if (eventId === 4) {
+    price = 300;
+  }
+  
+  let total = price * ticknum;
+  return total;
 }
-
-
--- validate.js
+++ validate.js
@@ -1,35 +1,44 @@
 function checkNID() {
-  let nid = (document.getElementById("nid").value).trim();
-  if (nid.length != 13) {
-    return false;
-  } else {
-	return true;
+	let nid = (document.getElementById("nid").value).trim();
+	if (nid.length != 13) {
+	  return false;
+	} else {
+	  return true;
+	}
   }
-}
-
-function checkTicketNo() {
-  let num = (document.getElementById("ticknum").value).trim();
-  if (isNaN(num)) {
-    return false;
-  } else {
-	return true;
+  
+  function checkTicketNo() {
+	let num = (document.getElementById("ticknum").value).trim();
+	if (isNaN(num)) {
+	  return false;
+	} else {
+	  return true;
+	}
   }
-}
-
-function validateForm(){
+  
+  function priceCalculate() {
+	let numTickets = parseInt(document.getElementById("ticknum").value);
+	let totalPrice = numTickets + pricePerTicket;
+	return totalPrice;
+  }
+  
+  function validateForm(){
 	if(!checkNID()){
 	  alert("Invalid value for National ID!");
 	  document.getElementById("nid").focus();
 	  return false;
-	}else{
-		if(!checkTicketNo()){
-		  alert("Invalid value for No.of tickets!!");
-		  document.getElementById("ticknum").focus();
-		  return false;
-		}else{
-			total = priceCalculate();
-			alert("Total price for this booking is "+total+" USD");
-			return false;
-		}
+	} else if(!checkTicketNo()){
+	  alert("Invalid value for No. of tickets!!");
+	  document.getElementById("ticknum").focus();
+	  return false;
+	} else if(parseInt(document.getElementById("ticknum").value) > 5){
+	  alert("Maximum 5 tickets can be booked at once!!");
+	  document.getElementById("ticknum").focus();
+	  return false;
+	} else {
+	  let total = priceCalculate();
+	  alert("Total price for this booking is " + total + " USD");
+	  return false;
 	}
-}
+  }
+  
